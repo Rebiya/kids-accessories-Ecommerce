ad45a97e89b915a3c7678137d86d4dbda4b1505c
Rebiya <rebum.19@gmail.com>
2025-05-12 13:14:48 +0300

    payment is updated

--- Backend/Index.js
+++ Backend/Index.js
@@ -3,30 +3,27 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const query = require("./Config/db.config")
-// Load environment variables from .env file
-dotenv.config();
-const expressSanitizer = require("express-sanitizer"); // Fixed sanitization issue
-// Allow all origins and handle preflight requests
+
+const expressSanitizer = require("express-sanitizer"); 
+
+
 const corsOptions = {
   origin: "*", // Allow all origins
   credentials: true, // Allows cookies and auth headers
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow all HTTP methods
-  preflightContinue: false, // Disable preflight caching
+  preflightContinue: false, 
   optionsSuccessStatus: 204, // Return 204 for preflight requests
 };
+
+
 // Initialize Stripe with the secret key from environment variables
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
 
-// Use CORS middleware to allow requests from any origin
 app.use(cors(corsOptions)); 
-
-
-// Parse incoming JSON requests
 app.use(express.json());
 app.use(expressSanitizer());
-// Import and use routes
+
 const router = require("./Routes/index.routes");
 app.use("/api", router);
 
